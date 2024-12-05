4ba0319042290e87ed5a6bb344a98c3a9ea1227a
Ali Özdemir <oezdemir.bielefeld@gmail.com>
2024-12-05 14:23:28 +0100

    create function to play a round and call it

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-const moves = ["rock", "paper", "scissors"];
+const   moves               =   [
+                                "rock", 
+                                "paper", 
+                                "scissors"
+                                ];
+let     humanScore          = 0;
+let     computerScore       = 0;
 
 
 const getComputerChoice = () => 
@@ -25,3 +31,42 @@ const getHumanChoice    = () =>
 }
 
 console.log(getHumanChoice());
+
+
+const playRound         = (humanChoice, computerChoice) =>
+{
+    let winMessage      =   "You win!";
+    let loseMessage     =   "You lose!";
+    let tieMessage      =   "It's a tie! Try again!"; 
+
+    if 
+    (
+        humanSelection == moves[0] && computerSelection == moves[2] ||
+        humanSelection == moves[1] && computerSelection == moves[0] ||
+        humanSelection == moves[2] && computerSelection == moves[1]
+    )
+    {
+        humanScore++;
+        return winMessage   + `${humanSelection} beats ${computerSelection}.`;
+    }
+    else if 
+    (
+        humanSelection == moves[0] && computerSelection == moves[1] ||
+        humanSelection == moves[1] && computerSelection == moves[2] ||
+        humanSelection == moves[2] && computerSelection == moves[0]
+    )
+    {
+        computerScore++;
+        return loseMessage  + `${humanSelection} is beaten by ${computerSelection}.`;
+    }
+    else
+    {
+        return tieMessage;
+    }
+}
+
+const   humanSelection      = getHumanChoice();
+const   computerSelection   = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(playRound(humanSelection, computerSelection));
