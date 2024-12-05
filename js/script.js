b1154a8ba6020b5b6b0755908e2907fc8a1fb38b
Ali Özdemir <oezdemir.bielefeld@gmail.com>
2024-12-05 13:21:09 +0100

    create random function and modify moves
    create function for computer player so he picks a random move
    put the values for the game into a single array

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-const rock      = "Rock";
-const paper     = "Paper";
-const scissors  = "Scissors";
+const moves = ["rock", "paper", "scissors"];
 
 const getComputerChoice = () => 
-    {
-        
-    };
+{
+    let randomChoice = Math.floor(Math.random() * moves.length.toString());
+    return moves[randomChoice];
+};
+
+console.log(getComputerChoice());
