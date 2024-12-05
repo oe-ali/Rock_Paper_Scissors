5a7896ccab8170896b755d06119d72c70533de26
Ali Özdemir <oezdemir.bielefeld@gmail.com>
2024-12-05 13:36:47 +0100

    create player choice function with prompt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const moves = ["rock", "paper", "scissors"];
 
+
 const getComputerChoice = () => 
 {
     let randomChoice = Math.floor(Math.random() * moves.length.toString());
@@ -7,3 +8,20 @@ const getComputerChoice = () =>
 };
 
 console.log(getComputerChoice());
+
+
+const getHumanChoice    = () => 
+{
+    let action = prompt("What is your move?");
+
+    if (action != ("rock" || "paper" || "scissors"))
+    {
+        alert("You may only enter the words 'rock', 'paper' or 'scissors'!");
+    }
+    else
+    {
+        return action;
+    }
+}
+
+console.log(getHumanChoice());
